2cd52d8542aa6059bdb636fb039830ab95f707a2
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2023-09-24 22:15:53 +0600

    Feat: reservation model added

--- models/reservation.model.js
+++ models/reservation.model.js
@@ -1,6 +1,33 @@
 const mongoose = require("mongoose");
 
 const reservationSchema = new mongoose.Schema({
+    listingId: {
+        type: String
+    },
+    authorId: {
+        type: String
+    },
+    checkIn: {
+        type: String
+    },
+    checkOut: {
+        type: String
+    },
+    nightStaying: {
+        type: Number
+    },
+    guestNumber: {
+        type: Number
+    },
+    basePrice: {
+        type: Number
+    },
+    taxes: {
+        type: Number
+    },
+    authorEarnedPrice: {
+        type: Number
+    }
 
 }, { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } })
 
